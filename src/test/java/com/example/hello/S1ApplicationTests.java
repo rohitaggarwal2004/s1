e4b2a1267dd8d44fcd2e14ebba83e4ae0d595d78
rohitaggarwal2004 <34626770+rohitaggarwal2004@users.noreply.github.com>
2019-12-26 16:01:58 +0530

    Add files via upload
    Commit

---
+++ src/test/java/com/example/hello/S1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
