f48ad311cba8bd20605ec490405b6f385f30da4b
rohit aggarwal <rohit.aggarwal@rohitaggarwal.net>
2020-01-05 15:17:53 +0530

    adsad

--- src/main/java/com/example/hello/S2Proxy.java
+++ src/main/java/com/example/hello/S2Proxy.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //@FeignClient(name="s2", url="localhost:8091")
-@FeignClient(name="s2")
+@FeignClient(name="apigateway")
 @RibbonClient(name="s2")
 public interface S2Proxy {
 	
-	@GetMapping("s2/bye")
+	@GetMapping("s2/s2/bye")
 	public String s2Bye();
 }
